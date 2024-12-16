a48903fa6140a03e50e8f14fbf763eb31f824f96
Hermyes <aslan.ergaliev22@mail.ru>
2024-12-16 17:18:20 +0300

    2

--- dnd-app/src/api/index.ts
+++ dnd-app/src/api/index.ts
@@ -1,6 +1,6 @@
 import { Api } from './Api';
 
 export const api = new Api({
-    baseURL: 'http://172.20.10.3:3000/api', // менять когда меняешь вайфай
+    baseURL: 'http://192.168.1.33:8000/api', // менять когда меняешь вайфай
     withCredentials: true
 });
