09d8acc4af47cd5b1eadafb534672d8847e8bc0e
Hermyes <aslan.ergaliev22@mail.ru>
2024-12-16 12:31:55 +0300

    Изменяю айпи чтобы потом вернуться

--- dnd-app/vite.config.ts
+++ dnd-app/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       "/api": {
-        target: "http://192.168.1.33:8000", // менять когда меняешь вайфай
+        target: "http://172.20.10.3:8000", // менять когда меняешь вайфай
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       }
